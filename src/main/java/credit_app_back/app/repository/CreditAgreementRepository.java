ad40249003a6e3e4974dea28abb82c0194d104e0
SergeyGT <sergeyburdenyuk@mail.ru>
2026-08-08 14:08:05 +0300

    fix: добавлены отсутствующие аннотации к репозиториям

--- src/main/java/credit_app_back/app/repository/CreditAgreementRepository.java
+++ src/main/java/credit_app_back/app/repository/CreditAgreementRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import credit_app_back.app.entity.CreditAgreement;
 
+@Repository
 public interface CreditAgreementRepository extends JpaRepository<CreditAgreement, Long> {
     Optional<CreditAgreement> findById(Long id);
     List<CreditAgreement> findAll();
--- src/main/java/credit_app_back/app/repository/CreditApplicationRepository.java
+++ src/main/java/credit_app_back/app/repository/CreditApplicationRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import credit_app_back.app.entity.CreditApplication;
 import credit_app_back.app.entity.CreditApplicationStatus;
 
+@Repository
 public interface CreditApplicationRepository extends JpaRepository<CreditApplication, Long> {
     Optional<CreditApplication> findById(Long id);
     List<CreditApplication> findAll();
