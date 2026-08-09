99fbf4675c84d3818e35f061b501696d3aeb39aa
SergeyGT <sergeyburdenyuk@mail.ru>
2026-08-10 01:39:28 +0300

    feat: добавлен dto для поиска клиентов по параметрам

---
+++ src/main/java/credit_app_back/app/dto/FindClientsDto.java
@@ -0,0 +1,19 @@
+package credit_app_back.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FindClientsDto {
+
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String passport;
+    private String phoneNumber;
+}
